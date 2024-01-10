695daac0b5c15a206b6cf214ee23c41b10f5e3d9
orsolyahrubo <orsolya.hrubo@gmail.com>
2024-01-10 18:37:45 +0100

    Gamecontext with more states

--- app/src/contexts/GameContext.js
+++ app/src/contexts/GameContext.js
@@ -2,12 +2,16 @@ import { createContext, useState, useEffect } from 'react';
 
 export const GameContext = createContext();
 
-export default function DogsContextProvider({ children }) {
+export default function GameContextProvider({ children }) {
 
     const [errormessage, setErrorMessage] = useState(null);
     const [currentBoard, setCurrentBoard] = useState(Array(15).fill(Array(15).fill(null)));
+    const [currentPlayer, setCurrentPlayer] = useState(null);
+    const [currentTile, setCurrentTile] = useState(null);
+    const [currentMove, setCurrentMove] = useState(null);
 
-    useEffect(() => {
+
+    async function makeFakeBoard() {
         const fakeTile = { value: 'A', score: 1 };
         setCurrentBoard(currentBoard => {
             const newBoard = currentBoard.map((row, rowIndex) => {
@@ -17,16 +21,19 @@ export default function DogsContextProvider({ children }) {
                     }
                     return element;
                 });
-            }
-            );
+            });
             return newBoard;
         });
+    }
+
+    useEffect(() => {
+        makeFakeBoard();
     }, []);
 
     return (
         // eslint-disable-next-line react/jsx-no-constructed-context-values
         <GameContext.Provider value={{
-            errormessage, setErrorMessage, currentBoard, setCurrentBoard
+            errormessage, setErrorMessage, currentBoard, setCurrentTile
         }}
         >
             {children}
