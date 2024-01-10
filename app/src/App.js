dbeef5e11a00c2e6afcc0620f9b8d7b43ec2902a
orsolyahrubo <orsolya.hrubo@gmail.com>
2024-01-10 13:08:29 +0100

    FakeCurrentBoard has been created to try out Boards props

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,7 @@
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap';
+import { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import NotFound from './pages/NotFound';
@@ -12,15 +13,41 @@ import ScrabbleHand from './components/ScrabbleHand';
 import ScrabbleBoard from './components/ScrabbleBoard';
 
 function App() {
+  const [currentBoard, setCurrentBoard] = useState(Array(15).fill(Array(15).fill(null)));
+  // const [arrOfPlayersCurrentLetters, setArrOfPlayersCurrentLetters] = useState([]);
+
+  // useEffect(() => {
+  //   fetch('http://localhost:3001/api/board')
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       setCurrentBoard(data);
+  //     });
+  // }, [currentBoard]);
+
+  useEffect(() => {
+    const fakeTile = { value: 'A', score: 1 };
+    setCurrentBoard(currentBoard => {
+      const newBoard = currentBoard.map((row, rowIndex) => {
+        return row.map((element, columnIndex) => {
+          if (rowIndex === 3 && columnIndex === 2) {
+            return fakeTile;
+          }
+          return element;
+        });
+      }
+      );
+      return newBoard;
+    });
+  }, []);
+
   return (
     <BrowserRouter>
       <div className="page-container">
         <div className="content-wrap">
           <Header />
           <ScrabbleTile value={'B'} score={2} />
-          <ScrabbleTile />
           <ScrabbleHand arrOfPlayersCurrentLetters={[{ value: 'C', score: 3 }, { value: 'D', score: 4 }, { value: 'C', score: 3 }, { value: 'D', score: 4 }, { value: 'C', score: 3 }, { value: 'D', score: 4 }, { value: 'C', score: 3 }, { value: 'D', score: 4 }]} />
-          <ScrabbleBoard />
+          <ScrabbleBoard currentBoard={currentBoard} />
           <Routes>
             <Route path="*" element={<NotFound />} />
             <Route path="/" element={<Home />} />
--- app/src/components/ScrabbleBoard.js
+++ app/src/components/ScrabbleBoard.js
@@ -1,15 +1,13 @@
 import './ScrabbleBoard.scss'
 import ScrabbleTile from './ScrabbleTile'
 
-export default function ScrabbleBoard() {
-
-    const currentBoard = Array(15).fill(Array(15).fill(null))
+export default function ScrabbleBoard({currentBoard}) {
     
     return (
         <div className='board container border d-flex flex-wrap text-center'>
             {currentBoard.map((row) => (
-                row.map((element) => (
-                    <ScrabbleTile value={element?.value} score={element?.score} />
+                row.map((element, index) => (
+                    <ScrabbleTile key={index} value={element?.value} score={element?.score} />
                 ))
             ))}
         </div>
