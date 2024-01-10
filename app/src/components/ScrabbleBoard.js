f119dc168c85acf86ecd56bc82873c11440feb48
orsolyahrubo <orsolya.hrubo@gmail.com>
2024-01-10 15:18:46 +0100

    Board is shown 15x15 always

--- app/src/components/ScrabbleBoard.js
+++ app/src/components/ScrabbleBoard.js
@@ -4,7 +4,7 @@ import ScrabbleTile from './ScrabbleTile'
 export default function ScrabbleBoard({currentBoard}) {
     
     return (
-        <div className='board container border d-flex flex-wrap text-center'>
+        <div className='board container-xl border d-flex flex-wrap text-center'>
             {currentBoard.map((row) => (
                 row.map((element, index) => (
                     <ScrabbleTile key={index} value={element?.value} score={element?.score} />
