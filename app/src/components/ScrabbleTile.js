dcdc51a247ce08b086af87108b08aa17d89de80d
orsolyahrubo <orsolya.hrubo@gmail.com>
2024-01-16 16:54:56 +0100

    Using uuid package instead of react-uuid

--- app/src/components/ScrabbleTile.js
+++ app/src/components/ScrabbleTile.js
@@ -1,5 +1,5 @@
 import './ScrabbleTile.scss'
-import uuid from 'react-uuid';
+import { v4 as uuidv4 } from 'uuid';
 import { useContext } from 'react';
 import { GameContext } from '../contexts/GameContext';
 
@@ -10,7 +10,7 @@ export default function ScrabbleTile({ value, score }) {
     return (
         <>
             {value && score ? (
-                <div id={uuid()} className="tile btn btn-secondary d-inline-flex m-1" onClick={() => getCurrentTile(value, score)}>
+                <div id={uuidv4()} className="tile btn btn-secondary d-inline-flex m-1" onClick={() => getCurrentTile(value, score)}>
                     <p className="px-1 mr-0 w-100 fs-4">{value}</p>
                     <p className="pt-4 pb-0 m-0 flex-shrink-1">{score}</p>
                 </div>
