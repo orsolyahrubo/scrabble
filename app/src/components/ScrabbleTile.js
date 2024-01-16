87ffd55426d10060c363fa302aebc2cba582bce4
orsolyahrubo <orsolya.hrubo@gmail.com>
2024-01-16 14:29:47 +0100

    Every tile has an auto generated unique id.

--- app/src/components/ScrabbleTile.js
+++ app/src/components/ScrabbleTile.js
@@ -10,7 +10,7 @@ export default function ScrabbleTile({ value, score }) {
     return (
         <>
             {value && score ? (
-                <div className="tile btn btn-secondary d-inline-flex m-1" onClick={() => getCurrentTile(value, score)}>
+                <div id={uuid()} className="tile btn btn-secondary d-inline-flex m-1" onClick={() => getCurrentTile(value, score)}>
                     <p className="px-1 mr-0 w-100 fs-4">{value}</p>
                     <p className="pt-4 pb-0 m-0 flex-shrink-1">{score}</p>
                 </div>
