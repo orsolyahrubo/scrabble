d7e3ebd7cd71280d76d1df1ba218668e2113a88f
orsolyahrubo <orsolya.hrubo@gmail.com>
2024-02-02 15:58:40 +0100

    Errorhandler middleware has been added

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import { stream } from './logger';
 import { api, swagger } from './routes/index';
+import errorHandler from './middlewares/error-handler';
 
 const app = express();
 
@@ -9,5 +10,7 @@ app.use(morgan('combined', { stream }));
 
 app.use('/api', api);
 app.use('/api-docs', swagger);
+
+app.use(errorHandler);
 
 export default app;
