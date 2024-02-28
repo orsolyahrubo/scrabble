fae0c1cbfaa4c782d11e98a8e0257482d6116684
orsolyahrubo <orsolya.hrubo@gmail.com>
2024-02-28 16:38:39 +0100

    refactoring schema but not the final solution

--- api/src/game/game.model.ts
+++ api/src/game/game.model.ts
@@ -1,4 +1,5 @@
 import mongoose, { Types } from "mongoose";
+import { platform } from "os";
 
 export type Tile = {
     value: string,
@@ -27,7 +28,7 @@ export type BoardState = {
 }
 
 export type Game = {
-    players: Player[],
+    players: Types.DocumentArray<Player>,
     bag: Tile[],
     moves: Move[],
     currentTurn: string,
@@ -35,11 +36,16 @@ export type Game = {
 }
 
 const gameSchema = new mongoose.Schema<Game>({
-    players: [{ type: mongoose.Schema.Types.Array, ref: 'Player', required: true }],
-    bag: [{ type: mongoose.Schema.Types.Array, ref: 'Tile', required: true }],
-    moves: [{ type: mongoose.Schema.Types.Array, ref: 'Move', required: true }],
+    players: [{ userId: { type: String, required: true }, currentHand: [{ value: { type: String, required: true }, score: { type: Number, required: true } }] }],
+    bag: [{ value: { type: String, required: true }, score: { type: Number, required: true } }],
+    moves: [{ userId: { type: String, required: true }, tile: { value: { type: String, required: true }, score: { type: Number, required: true } }, position: { x: { type: Number, required: true }, y: { type: Number, required: true } } }],
     currentTurn: { type: String, required: true },
-    board: [{ type: mongoose.Schema.Types.Array, ref: 'BoardState', required: true }],
+    board: [{ tile: { value: { type: String, required: true }, score: { type: Number, required: true } }, position: { x: { type: Number, required: true }, y: { type: Number, required: true } } }],
+    // players: [{ type: mongoose.Schema.Types.Array, ref: 'Player', required: true }],
+    // bag: [{ type: mongoose.Schema.Types.Array, ref: 'Tile', required: true }],
+    // moves: [{ type: mongoose.Schema.Types.Array, ref: 'Move', required: true }],
+    // currentTurn: { type: String, required: true },
+    // board: [{ type: mongoose.Schema.Types.Array, ref: 'BoardState', required: true }],
 },
     {
         toJSON: {
@@ -61,3 +67,60 @@ const gameSchema = new mongoose.Schema<Game>({
     });
 
 export const GameModel = mongoose.model<Game>("GameModel", gameSchema, "games");
+
+export function createGame() {
+    const exampleGame = {
+        players: [
+            {
+                userId: "1",
+                currentHand: [
+                    { value: "A", score: 1 },
+                    { value: "B", score: 2 },
+                    { value: "C", score: 3 },
+                    { value: "D", score: 4 },
+                    { value: "E", score: 5 },
+                    { value: "F", score: 6 },
+                    { value: "G", score: 7 },
+                ]
+            },
+            {
+                userId: "2",
+                currentHand: [
+                    { value: "A", score: 1 },
+                    { value: "B", score: 2 },
+                    { value: "C", score: 3 },
+                    { value: "D", score: 4 },
+                    { value: "E", score: 5 },
+                    { value: "F", score: 6 },
+                    { value: "G", score: 7 },
+                ]
+            }
+        ],
+        bag: [
+            { value: "A", score: 1 },
+            { value: "B", score: 2 },
+            { value: "C", score: 3 },
+            { value: "D", score: 4 },
+            { value: "E", score: 5 },
+            { value: "F", score: 6 },
+            { value: "G", score: 7 },
+        ],
+        moves: [
+            {
+                userId: "1",
+                tile: { value: "A", score: 1 },
+                position: { x: 0, y: 0 }
+            }
+        ],
+        currentTurn: "1",
+        board: [
+            {
+                tile: { value: "A", score: 1 },
+                position: { x: 0, y: 0 }
+            }
+        ]
+    }
+
+    GameModel.create(exampleGame);
+
+}
